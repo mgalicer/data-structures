270aebc50762be8236aed6da8a7c7f0e7d56f1fe
marigalicer <marigalicer@gmail.com>
2018-09-23 18:23:09 -0400

    complete size, maxdepth and minvalue methods

--- BST/BST.java
+++ BST/BST.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class BST {
     private Node root;
 
@@ -48,16 +50,47 @@ public class BST {
         return node;
     }
 
-    private int size() {
-
+    public int size() {
+        return size(root);
     }
+
+    private int size(Node node) {
+        if(node == null) return 0;
+        return 1 + size(node.left) + size(node.right);
+    }
+
+    public int maxDepth() {
+        return maxDepth(root);
+    }
+
+    private int maxDepth(Node node) {
+        if(node == null) return 0;
+        return Math.max(1 + maxDepth(node.left), 1 + maxDepth(node.right));
+    }
+
+    public int minValue() {
+        return minValue(root);
+    }
+
+    private int minValue(Node node) {
+        if(node.left == null) return node.data;
+        return minValue(node.left);
+    }
+
+
 
     public static void main(String args[]) {
         BST binSearchTree = new BST();
         binSearchTree.insert(5);
         binSearchTree.insert(10);
-        binSearchTree.insert(3);
-        System.out.println(binSearchTree);
+        binSearchTree.insert(1);
+        binSearchTree.insert(8);
+        binSearchTree.insert(4);
+
+
+
+        System.out.println(binSearchTree.minValue());
+
     }
 
 }
