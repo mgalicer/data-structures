831362eccfd1137bed2135c4616fb8d4df516597
marigalicer <marigalicer@gmail.com>
2018-10-14 12:15:24 -0400

    put method

--- HashTable/src/java/SeparateChainedList.java
+++ HashTable/src/java/SeparateChainedList.java
@@ -21,15 +21,27 @@ public class SeparateChainedList<K, V> {
     }
 
     public void delete(K key) {
+        // first check if key is null or list is empty
         if(key == null) throw new IllegalArgumentException("Delete method does not accept null values");
+        if(size == 0) return;
+
+        // if list has only one node to be removed, remove it
         if(head.key.equals(key)) {
-            head = head.next;
-            return;
+            head = null;
+            size--;
         }
 
         Node current = head;
-        while(current != null) {
-            if(current.)
+        Node next = current.next;
+
+        while(next != null && !(next.key.equals(key))) {
+            current = next;
+            next = next.next;
+        }
+
+        if(next != null) {
+            current.next = next.next;
+            size--;
         }
     }
 
@@ -47,14 +59,37 @@ public class SeparateChainedList<K, V> {
 
     public boolean isEmpty() { return size == 0; }
 
-    public V put(K key, V value) {
+    public void put(K key, V val) {
+        if(val == null) throw new IllegalArgumentException("Put method does not accept null values");
+
+        Node current = head;
+
+        while(current != null) {
+            if((current.key).equals(key)) {
+                current.value = val;
+                return;
+            }
+            current = current.next;
+        }
+
+        head = new Node(key, val, head);
 
     }
 
     public int size() { return size; }
 
     public String toString() {
+        if(head == null) return "";
+        Node current = head;
 
+        StringBuilder listAsString = new StringBuilder();
+
+        while(current != null) {
+            listAsString.append(current.key + "=" + current.value + ", ");
+            current = current.next;
+        }
+
+        return listAsString.toString();
     }
 
 }
